5a053730f748367d8489d898c4156c3437e60d0a
DaviLima <daviunb25@gmail.com>
2021-09-05 20:52:53 -0300

    Criando classe Produto
    Co-authored-by: Karla Feliciano <200021541@aluno.unb.br>

---
+++ src/models/Produto.java
@@ -0,0 +1,66 @@
+package models;
+
+public abstract class Produto {
+	
+	private int idProduto;
+	private String nomeP;
+	private String descricao;
+	private String cor;
+	private double preco; 
+	private double tamanho; 
+	private String fabricante;
+
+	public Produto(int idProduto, String nomeP, String cor, double preco, double tamanho, String fabricante) {
+		this.idProduto = idProduto;
+		this.nomeP = nomeP;
+		this.cor = cor;
+		this.preco = preco;
+		this.tamanho = tamanho;
+		this.fabricante = fabricante;	
+	}
+	
+	
+	public int getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(int idProduto) {
+		this.idProduto = idProduto;
+	}
+	public String getNomeP() {
+		return nomeP;
+	}
+	public void setNomeP(String nomeP) {
+		this.nomeP = nomeP;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public double getTamanho() {
+		return tamanho;
+	}
+	public void setTamanho(double tamanho) {
+		this.tamanho = tamanho;
+	}
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	} 
+
+}
