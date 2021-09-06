58983aa109520274ad892abb1a5bc8d503ef110c
Karla <200021541@aluno.unb.br>
2021-09-06 00:06:24 -0300

    Criação da classe Telefone

---
+++ src/models/Telefone.java
@@ -0,0 +1,30 @@
+package models;
+
+public class Telefone {
+
+    private String ddd;
+    private String numero;
+
+    public Telefone(String ddd, String numero) {
+        this.ddd = ddd;
+        this.numero = numero;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+
+}
