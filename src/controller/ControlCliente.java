aad1bc0dfe92202f4291bcce2f1d93d4c08df5dd
DaviLima <daviunb25@gmail.com>
2021-09-30 21:28:24 -0300

    Criacao de metodos do controlCliente
    Co-authored-by: Karla Feliciano <200021541@aluno.unb.br>"

--- src/controller/ControlCliente.java
+++ src/controller/ControlCliente.java
@@ -1,19 +1,31 @@
 package controller;
-import models.*;
+import models.Cartao;
+import models.Cliente;
+import models.Endereco;
+import models.Telefone;
 import java.util.*;
 
 
 public class ControlCliente {
     private static List<String[]> dadosCliente = new ArrayList<>();
+    private static List<String> dadosViewCliente = new ArrayList<>();
     private static String cartao;
+    private static ControlRegisto dadosR;
+
+    static {
+        try {
+            dadosR = new ControlRegisto();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
 
     public ControlCliente(){
     }
 
-
     //METODO QUE DEFINE OS ELEMENTOS QUE IRAO APARECER NA TABELA
     public static List<String[]> dadosTabela(ControlRegisto clienteD){
-
+        dadosCliente.clear();
         for(int i=0;i< clienteD.getDados().getCliente().size(); i++){
             if(clienteD.getDados().getCliente().get(i).getCartao() != null)
                 cartao = "SIM";
@@ -26,221 +38,127 @@ public class ControlCliente {
                                       clienteD.getDados().getCliente().get(i).getTelefone().toString(),
                                       cartao});
         }
-
     return dadosCliente;
+    }
+
+    //METODO QUE DEFINE OS ELEMENTOS QUE IRAO APARECER NA TABELA APOS O FILTRO
+    public static List<String[]> filtrarTabela(ControlRegisto clienteD, String nome){
+        dadosCliente.clear();
+        for(int i=0;i< clienteD.getDados().getCliente().size(); i++){
+            if(nome.equals(clienteD.getDados().getCliente().get(i).getNome())){
+                if(clienteD.getDados().getCliente().get(i).getCartao() != null)
+                    cartao = "SIM";
+                else
+                    cartao = "NAO";
+
+                dadosCliente.add(new String[]{clienteD.getDados().getCliente().get(i).getNome(),
+                        clienteD.getDados().getCliente().get(i).getCpf(),
+                        clienteD.getDados().getCliente().get(i).getEndereco().getUf(),
+                        clienteD.getDados().getCliente().get(i).getTelefone().toString(),
+                        cartao});
+            }
+        }
+        return dadosCliente;
+
+    }
+    //METODO PARA ADCIONAR CLIENTE
+    public static void adicionarCliente(List<String> dadosCliente){
+        Cliente novoCliente = new Cliente();
+        Telefone novoTelefone = new Telefone();
+        Endereco novoEndereco = new Endereco();
+        Cartao novoCartao = new Cartao();
+
+        novoCliente.setNome(dadosCliente.get(0));
+        novoCliente.setCpf(dadosCliente.get(1));
+        novoCliente.setDataNascimento(dadosCliente.get(2));
+
+        novoTelefone.setDdd(dadosCliente.get(3));
+        novoTelefone.setNumero(dadosCliente.get(4));
+        novoCliente.setTelefone(novoTelefone);
+
+        novoEndereco.setUf(dadosCliente.get(5));
+        novoEndereco.setCidade(dadosCliente.get(6));
+        novoEndereco.setBairro(dadosCliente.get(7));
+        novoEndereco.setLogradouro(dadosCliente.get(8));
+        novoCliente.setEndereco(novoEndereco);
+
+        novoCartao.setNome(dadosCliente.get(9));
+        novoCartao.setNumero(dadosCliente.get(10));
+        novoCartao.setDataVencimento(dadosCliente.get(11));
+        novoCartao.setCvv(dadosCliente.get(12));
+        novoCliente.setCartao(novoCartao);
+
+        ControlRegisto.getDados().getCliente().add(novoCliente);
+    }
+
+    //METODO PARA ADQUIRIR O DADOS PARA A TELA DE UM CLIENTE
+    public static List<String> pegarDadosCliente(ControlRegisto clienteD,String cpf){
+        int i = 0;
+        dadosViewCliente.clear();
+        for(int index=0;index< clienteD.getDados().getCliente().size(); index++)
+            if(clienteD.getDados().getCliente().get(index).getCpf().equals(cpf))
+                i = index;
+
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getNome());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getCpf());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getDataNascimento());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getTelefone().getDdd());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getTelefone().getNumero());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getEndereco().getUf());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getEndereco().getCidade());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getEndereco().getBairro());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getEndereco().getLogradouro());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getCartao().getNome());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getCartao().getNumero());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getCartao().getDataVencimento());
+            dadosViewCliente.add(clienteD.getDados().getCliente().get(i).getCartao().getCvv());
+
+
+        return dadosViewCliente;
+    }
+
+    //METODO PARA ALTERAR O DADOS DA TELA DE UM CLIENTE
+    public static void alterarDadosCliente(ControlRegisto clienteD,String cpf, List<String> dadosCliente){
+        List<String> alteraDados = new ArrayList<>(dadosCliente);
+
+        int i = 0;
+        for(int index=0;index< clienteD.getDados().getCliente().size(); index++)
+            if(clienteD.getDados().getCliente().get(index).getCpf().equals(cpf))
+                i = index;
+
+        clienteD.getDados().getCliente().get(i).setNome(alteraDados.get(0));
+        clienteD.getDados().getCliente().get(i).setCpf(alteraDados.get(1));
+        clienteD.getDados().getCliente().get(i).setDataNascimento(alteraDados.get(2));
+        clienteD.getDados().getCliente().get(i).getTelefone().setDdd(alteraDados.get(3));
+        clienteD.getDados().getCliente().get(i).getTelefone().setNumero(alteraDados.get(4));
+        clienteD.getDados().getCliente().get(i).getEndereco().setUf(alteraDados.get(5));
+        clienteD.getDados().getCliente().get(i).getEndereco().setCidade(alteraDados.get(6));
+        clienteD.getDados().getCliente().get(i).getEndereco().setBairro(alteraDados.get(7));
+        clienteD.getDados().getCliente().get(i).getEndereco().setLogradouro(alteraDados.get(8));
+        clienteD.getDados().getCliente().get(i).getCartao().setNome(alteraDados.get(9));
+        clienteD.getDados().getCliente().get(i).getCartao().setNumero(alteraDados.get(10));
+        clienteD.getDados().getCliente().get(i).getCartao().setDataVencimento(alteraDados.get(11));
+        clienteD.getDados().getCliente().get(i).getCartao().setCvv(alteraDados.get(12));
+    }
+
+    //METODO PARA ALTERAR O DADOS DA TELA DE UM CLIENTE
+    public static void excluirCliente(ControlRegisto clienteD,String cpf, List<String> dadosCliente){
+        List<String> alteraDados = new ArrayList<>(dadosCliente);
+
+        int i = 0;
+        for(int index=0;index< clienteD.getDados().getCliente().size(); index++)
+            if(clienteD.getDados().getCliente().get(index).getCpf().equals(cpf))
+                i = index;
+        clienteD.getDados().getCliente().remove(i);
+    }
+
+    public static ControlRegisto getDadosR() {
+        return dadosR;
+    }
+
+    public static void setDadosR(ControlRegisto dadosR) {
+        ControlCliente.dadosR = dadosR;
     }
 }
 
 
-
-
-/*
-//02 METODOS DO CLIENTE
-    public static void CLIENTE(Registro dado){
-        //VARIAVEIS DE ESCOLHAS E ARMAZENAMENTO
-        int auxCli = 0;
-        String opcaoS;
-        Cliente cpfCliente = new Cliente();
-        List<Cliente> clienteAux = new ArrayList<>();
-        List<String> op = new ArrayList<>(Arrays.asList("1", "2", "3", "4"));
-
-
-
-        //ALTERAR/DELETAR/BUSCAR UM CLIENTE
-        else if(opcaoS.equals(op.get(1))){
-            System.out.println(" Digite 1 para filtrar por nome(apenas consultar)");
-            System.out.println(" Digite 2 para filtrar por cpf(alterar/deletar)");
-            opcaoS = ler.nextLine();
-
-            //VALIDA  A OPCAO ACIMA
-            while (!op.subList(0, 2).contains(opcaoS)) {
-                System.out.println(" OPCAO INVALIDA!");
-
-                opcaoS = ler.nextLine();
-            }
-
-            //opcaoS = 1 = FILTRAR POR NOME(APENAS BUSCAS)
-            if (opcaoS.equals("1")) {
-                do {
-                    System.out.println(" Digite o nome do cliente a ser buscado");
-                    opcaoS = ler.nextLine();
-
-                    for (int i = 0; i < dado.getCliente().size(); i++)
-                        if (opcaoS.equalsIgnoreCase(dado.getCliente().get(i).getNome()))    // COMPARA O NOME INSERIDO COM TODOS CONTIDOS NO SISTEMA
-                            clienteAux.add(dado.getCliente().get(i));   // SE HOUVER ALGUM IGUAL O CLIENTE DA POSICAO EH ARMAZENADO NUM ARRAYLIST
-
-                    if (clienteAux.size() == 0)
-                        System.out.println(" Nao ha cliente com nome de"+opcaoS+ "no sistema");     //CASO NAO HAJA CLIENTE COM O NOME INSERIDO
-                    else
-                        for (Cliente clienteAu : clienteAux)
-                            System.out.println(clienteAu.toString().replace("[", "").replace("]", ""));     //CASO HAJA SERA IMPRESSO NA SAIDA
-
-                    System.out.println(" Deseja buscar outro nome no sistema?");
-                    System.out.println(" Digite: SIM ou NAO");
-                    opcaoS = ler.nextLine();
-
-                    //VALIDA A OPCAO
-                    while (!opcaoS.toLowerCase().contains("nao") && !opcaoS.toLowerCase().contains("sim")) {
-                        System.out.println(" OPCAO INVALIDA!");
-                        System.out.println(" Deseja buscar outro nome no sistema?");
-                        System.out.println(" Digite: SIM ou NAO");
-                        opcaoS = ler.nextLine();
-                    }
-
-                    clienteAux.clear();
-
-                } while (!opcaoS.toLowerCase().contains("nao"));
-            }
-            //FILTRO POR CPF(POSSIVEL DELETAR/ALTERAR/BUSCAR)
-            else {
-                do {
-                    System.out.println(" Digite o CPF do cliente que deseja buscar:");
-                    opcaoS = ler.nextLine();
-
-                    for (int i = 0; i < dado.getCliente().size(); i++) {
-                        if (opcaoS.equals(dado.getCliente().get(i).getCpf())){      //COMPARA O CPF INSERIDO COM O DE TODOS OS CLIENTE NO SISTEMA
-                            cpfCliente = dado.getCliente().get(i);      //ATRIBUI O VALOR DO CLIENTE PARA cpfCliente
-                            auxCli = i;     //ATRIBUI A auxCli o valor da posicao do cliente
-                        }
-                    }
-                    if(auxCli == 0) //SE NAO ACHAR NENHUM CPF IGUAL:
-                        System.out.println(" Nao ha cliente com o cpf"+opcaoS+" no sistema");
-                    else {
-                        System.out.println(cpfCliente.toString());
-                        System.out.println(" Deseja alterar ou deletar o cliente?");
-                        System.out.println(" [1] - DELETAR");
-                        System.out.println(" [2] - ALTERAR");
-                        System.out.println(" Digite o numero de alguma opcao ou qualquer coisa para retornar ao menu:");
-                        opcaoS = ler.nextLine();
-
-                        // opcaoS = 1 = DELETAR
-                        if (opcaoS.equals("1"))
-                            dado.getCliente().remove(auxCli);
-                            // opcaoS = 2 = ALTERAR
-                        else if(opcaoS.equals("2")){
-                            System.out.println(cpfCliente);
-                            System.out.println(" Informacoes do cliente, eh possivel editar Cartao, Telefone e endereco");
-
-                            //DADOS DO CARTAO
-                            System.out.println(" Digite os novos dados do Cartao");
-                            System.out.println(" Nome do cartao");
-                            opcaoS = ler.nextLine();
-                            cpfCliente.getCartao().setNome(opcaoS);
-                            System.out.println(" CVV");
-                            opcaoS = ler.nextLine();
-                            cpfCliente.getCartao().setCvv(opcaoS);
-                            System.out.println(" Numero do cartao");
-                            opcaoS = ler.nextLine();
-                            cpfCliente.getCartao().setNumero(opcaoS);
-                            System.out.println(" Data de vencimento");
-                            opcaoS = ler.nextLine();
-                            cpfCliente.getCartao().setDataVencimento(opcaoS);
-
-                            //DADOS DO ENDERECO
-                            System.out.println(" Digite os novos dados do endereco");
-                            System.out.println(" UF");
-                            opcaoS = ler.nextLine();
-                            cpfCliente.getEndereco().setUf(opcaoS);
-                            System.out.println(" cidade");
-                            opcaoS = ler.nextLine();
-                            cpfCliente.getEndereco().setCidade(opcaoS);
-                            System.out.println(" bairro");
-                            opcaoS = ler.nextLine();
-                            cpfCliente.getEndereco().setBairro(opcaoS);
-                            System.out.println(" logradouro");
-                            opcaoS = ler.nextLine();
-                            cpfCliente.getEndereco().setLogradouro(opcaoS);
-
-                            //DADOS DO TELEFONE
-                            System.out.println(" Digite os novos dados do telefone");
-                            System.out.println(" DDD");
-                            opcaoS = ler.nextLine();
-                            cpfCliente.getTelefone().setDdd(opcaoS);
-                            opcaoS = ler.nextLine();
-                            System.out.println(" Numero");
-                            cpfCliente.getTelefone().setNumero(opcaoS);
-
-                        }
-
-                    }
-
-                    System.out.println(" Deseja buscar por outro CPF no sistema?");
-                    System.out.println(" Digite: SIM ou NAO");
-                    opcaoS = ler.nextLine();
-
-                    //VALIDA A OPCAO ACIMA
-                    while (!opcaoS.toLowerCase().contains("nao") && !opcaoS.toLowerCase().contains("sim")) {
-                        System.out.println(" OPCAO INVALIDA!");
-                        System.out.println(" Deseja buscar outro CPF no sistema?");
-                        System.out.println(" Digite: SIM ou NAO");
-                        opcaoS = ler.nextLine();
-                    }
-                    cpfCliente = new Cliente();
-                } while (!opcaoS.toLowerCase().contains("nao"));
-            }
-        }
-        //CADASTRO DE NOVO CLIENTE
-        else if(opcaoS.equals(op.get(2))){
-            Cartao setCA = new Cartao();
-            Endereco setEnd = new Endereco();
-            Telefone setTel = new Telefone();
-
-            System.out.println(" [3] - CADASTRO DE NOVO CLIENTE");
-
-            System.out.println(" Digite o nome do cliente:");
-            opcaoS = ler.nextLine();
-            cpfCliente.setNome(opcaoS);
-
-            System.out.println(" Digite o CPF do cliente:");
-            opcaoS = ler.nextLine();
-            cpfCliente.setCpf(opcaoS);
-
-            System.out.println(" Digite a data de nascimento do cliente:");
-            opcaoS = ler.nextLine();
-            cpfCliente.setDataNascimento(opcaoS);
-
-            //DADOS DO CARTAO
-            System.out.println(" Digite os novos dados do Cartao");
-            System.out.println(" Nome do cartao");
-            opcaoS = ler.nextLine();
-            setCA.setNome(opcaoS);
-            System.out.println(" CVV");
-            opcaoS = ler.nextLine();
-            setCA.setCvv(opcaoS);
-            System.out.println(" Numero do cartao");
-            opcaoS = ler.nextLine();
-            setCA.setNumero(opcaoS);
-            System.out.println(" Data de vencimento");
-            opcaoS = ler.nextLine();
-            setCA.setDataVencimento(opcaoS);
-            cpfCliente.setCartao(setCA);
-
-            //DADOS DO ENDERECO
-            System.out.println(" Digite os novos dados do endereco");
-            System.out.println(" UF");
-            opcaoS = ler.nextLine();
-            setEnd.setUf(opcaoS);
-            System.out.println(" cidade");
-            opcaoS = ler.nextLine();
-            setEnd.setCidade(opcaoS);
-            System.out.println(" bairro");
-            opcaoS = ler.nextLine();
-            setEnd.setBairro(opcaoS);
-            System.out.println(" logradouro");
-            opcaoS = ler.nextLine();
-            setEnd.setLogradouro(opcaoS);
-            cpfCliente.setEndereco(setEnd);
-
-            //DADOS DO TELEFONE
-            System.out.println(" Digite os dados do telefone");
-            System.out.println(" DDD");
-            opcaoS = ler.nextLine();
-            setTel.setDdd(opcaoS);
-            System.out.println(" Numero");
-            opcaoS = ler.nextLine();
-            setTel.setNumero(opcaoS);
-            cpfCliente.setTelefone(setTel);
-
-            dado.getCliente().add(cpfCliente);
-        }
-    } */
